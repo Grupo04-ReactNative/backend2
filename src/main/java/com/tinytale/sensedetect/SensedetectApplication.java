2e7beafe4ec8aca12566ef7a6256a347d93a5ffd
wierdest <andrlzpt@protonmail.com>
2024-05-30 20:54:05 -0300

    primeiro commit

---
+++ src/main/java/com/tinytale/sensedetect/SensedetectApplication.java
@@ -0,0 +1,13 @@
+package com.tinytale.sensedetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensedetectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensedetectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinytale/sensedetect/SensedetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinytale.sensedetect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensedetectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
